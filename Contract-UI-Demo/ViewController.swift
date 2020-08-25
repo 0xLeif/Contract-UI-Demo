7ba91933145cd6d5a0c4687941de133bc00dc1e1
Zach Eriksen <zeriksen@clientresourcesinc.com>
2020-08-25 14:07:20 -0500

    Initial Commit

---
+++ Contract-UI-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contract-UI-Demo
+//
+//  Created by CRi on 8/25/20.
+//  Copyright © 2020 oneleif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
