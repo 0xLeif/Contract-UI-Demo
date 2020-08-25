b3da2d18a00135fbd6230b33be333e7bc8faeb8d
zmeriksen <zmeriksen@icloud.com>
2020-08-25 18:07:56 -0500

    Added SUIK ContractView example

--- Contract-UI-Demo/ViewController.swift
+++ Contract-UI-Demo/ViewController.swift
@@ -2,6 +2,12 @@ import UIKit
 import Later
 import SwiftUIKit
 
+struct LabelViewModel {
+    var text: String
+    var color: UIColor
+    var alignment: NSTextAlignment
+}
+
 class ViewController: UIViewController {
     let textContract = Contract<String>()
     let label = Label("❗️👀")
@@ -23,14 +29,43 @@ class ViewController: UIViewController {
         
         textContract.value = "Hello World"
         
+        let labelContractView = ContractView(view: Label("❗️👀")) { labelView in
+            Contract(initialValue: LabelViewModel(text: "Fetching...",
+                                                  color: .green,
+                                                  alignment: .center)) { vm in
+                Later.main {
+                    labelView
+                        .text(alignment: vm?.alignment ?? .left)
+                        .text(color: vm?.color ?? .black)
+                    
+                    labelView.text = vm?.text ?? "-1"
+                }
+            }
+        }
+        
+        Later.do(withDelay: 4) {
+            var vm = labelContractView.contract?.value
+            
+            vm?.text = "Hello...?"
+            vm?.alignment = .right
+            
+            labelContractView.contract?.value = vm
+        }
+        
         view.embed {
             VStack(distribution: .fillEqually) {
                 [
+                    labelContractView,
                     label
                         .text(alignment: .center)
                         .number(ofLines: 3),
-                    Spacer(),
-                    HStack(distribution: .fillEqually) {
+                    LoadingImage(URL(string: "https://avatars0.githubusercontent.com/u/8268288?s=460&u=2cb09673ea7f5230fa929b9b14a438cb2a65751c&v=4")!),
+                    Spacer()
+                ]
+            }
+            .configure { stack in
+                Later.main(withDelay: 3) {
+                    stack.views.value?.append(HStack(distribution: .fillEqually) {
                         [
                             Button("Resign Contract") { [weak self] in
                                 self?.textContract.resign()
@@ -40,8 +75,8 @@ class ViewController: UIViewController {
                             }
                         ]
                     }
-                    .padding()
-                ]
+                    .padding())
+                }
             }
         }
     }
